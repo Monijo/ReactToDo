e82e5242b239ca242e95646d3770533138311bb5
Monijo <mokajozefowicz@gmail.com>
2022-04-25 19:13:08 +0200

    Add ToDo component.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-
+import Todo from "./components/Todo";
 
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <h1>My todos</h1>
+    render() {
+        return (
+            <div className="App">
+                <h1>My Todos</h1>
+                <Todo text = "Zrobić apke w reactcie"/>
+                <Todo text="zamówić karme kotu"/>
+                <Todo text="Zrobic taski z django"/>
+                <Todo text="Poczytac o DL"/>
 
-
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default App;
